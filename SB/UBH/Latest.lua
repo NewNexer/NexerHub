0ad35985c78e938c0271d7119784aca2c7705026
NewNexer <nexer.dod.hub@gmail.com>
2026-07-24 02:53:43 +0900

    Update Latest.lua

--- SB/UBH/Latest.lua
+++ SB/UBH/Latest.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NewNexer/NexerHub/refs/heads/main/SB/UBH/Versions/1.2.0"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NewNexer/NexerHub/refs/heads/main/SB/UBH/Versions/1.3.0"))()
